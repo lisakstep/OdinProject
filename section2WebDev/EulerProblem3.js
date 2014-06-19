b78711e45042567b5eb2f9791ad7f5441274173f
Lisa Stephens <lisakstep@gmail.com>
2014-06-19 10:29:40 -0600

    Final version of solution. Some minimal optimization included.

--- section2WebDev/EulerProblem3.js
+++ section2WebDev/EulerProblem3.js
@@ -7,24 +7,39 @@ What is the largest prime factor of the number 600851475143 ?
 */
 
 //var targetNumber = 600851475143;
-var targetNumber = 1146;
+var targetNumber = 8;
 var workingTargetNumber = targetNumber;
 // Set up array for storing prime factors
 var pFactors = new Array();
+var allPrimeFactorsFound = false;
 
 /* 	Start looking for prime factors, brute force
 	Start with 2, since 1 is a factor for every number and isn't 
 	listed as a prime factor by convention.
 */
-console.log("Starting the for loop.");
 for ( var i = 2; i < targetNumber; i++) {
-    console.log("in the for loop. targetNumber is " + targetNumber + ". workingTargetNumber=" + workingTargetNumber + ". Number of prime factors found is " + pFactors.length);
+   // console.log("in the for loop. targetNumber is " + targetNumber + ". workingTargetNumber=" + workingTargetNumber + ". Number of prime factors found is " + pFactors.length);
     
-	while ( workingTargetNumber%i === 0) {
-	
+	while ( (workingTargetNumber%i === 0) && !allPrimeFactorsFound ) {
+		console.log("Factor found: " + i);
 		pFactors[pFactors.length] = i;
 		workingTargetNumber /= i;
 
+
+		/* Stop the search for factors when they are all found.
+			If factors have been found, see if when multiplied together
+			they = the target number. If so, your work is done :)
+		*/
+		if (pFactors.length > 1) {
+			var interimProduct = 1;
+			for (var j = 0; j < pFactors.length; j++) {
+				interimProduct *= pFactors[j];
+			};
+			console.log("Searching for factors. So far the interimProduct is " + interimProduct);
+			if (interimProduct === targetNumber) {
+				allPrimeFactorsFound = true;
+			}
+		}
     }
 }
 
