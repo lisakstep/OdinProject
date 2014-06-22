48ef3acadd80bed997b4b91fab6592991a9c3f0a
Lisa Stephens <lisakstep@gmail.com>
2014-06-22 17:44:21 -0600

    small div cells now change color when the mouse touches it

--- section2WebDev/browserSketch.js
+++ section2WebDev/browserSketch.js
@@ -1,19 +1,26 @@
 $(document).ready( function() {
 	// Set up the drawing field and cell parameters
-var numberOfSquares = 16;
-var drawingCellDimension = (500/Math.sqrt(numberOfSquares)) - 2*Math.sqrt(numberOfSquares);
+var numberOfSquares = 1600;
+var squaresPerSide = Math.sqrt(numberOfSquares);
+var widthOfField = 500-4;
+var drawingCellDimension = (widthOfField/squaresPerSide) - 2;
 	//alert("the jquery file");
 	//console.log("yep, i'm loaded.");
 // Add the wrapper div to the DOM
 $('body').append("<div class='wrapper'></div>");
 
-	// Draw the cells
+// Draw the cells
 for (var i = 0; i < numberOfSquares; i++) {
 	$('.wrapper').append('<div class="drawingCell"></div>');
 }
 
-	// Set the styling for the proper dimensions
-	$('.drawingCell').height(drawingCellDimension);	
-	$('.drawingCell').width(drawingCellDimension);	
+// Set the styling for the proper dimensions
+$('.drawingCell').height(drawingCellDimension);	
+$('.drawingCell').width(drawingCellDimension);	
 
+	//console.log("drawingCellDimension: " + drawingCellDimension );
+$('.drawingCell').on('mouseenter', function() {
+	$(this).css('background-color', '#0ff');
+
+	});
 });
