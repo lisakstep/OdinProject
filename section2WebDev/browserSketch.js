9c6d36195963bb7105420018a95548b39b09d5ba
Lisa Stephens <lisakstep@gmail.com>
2014-06-22 20:56:36 -0600

    start over button now works, but game not playable after pressing.

--- section2WebDev/browserSketch.js
+++ section2WebDev/browserSketch.js
@@ -10,17 +10,53 @@ var drawingCellDimension = (widthOfField/squaresPerSide) - 2;
 $('.wrapper').append("<div class='playingField'></div>");
 
 // Draw the cells
-for (var i = 0; i < numberOfSquares; i++) {
-	$('.playingField').append('<div class="drawingCell"></div>');
-}
+drawTheSquares();
 
 // Set the styling for the proper dimensions
-$('.drawingCell').height(drawingCellDimension);	
-$('.drawingCell').width(drawingCellDimension);	
+//$('.drawingCell').height(drawingCellDimension);	
+//$('.drawingCell').width(drawingCellDimension);	
 
 	//console.log("drawingCellDimension: " + drawingCellDimension );
+
+
+/************** Event handlers ****************************/
+	// Change the cell color when the mouse touches it
 $('.drawingCell').on('mouseenter', function() {
 	$(this).css('background-color', '#0ff');
-
 	});
+
+	// Handle button click for Start Over
+$('.startOver').on('click', function() {
+	// Hidejqu the current grid
+	deleteTheSquares();
+	getNumberOfSquaresPerSide();
+	console.log("new squaresPerSide as entered by user is "  + squaresPerSide);
+	drawTheSquares();
+});
+
+/************** Functions *********************************/
+
+function getNumberOfSquaresPerSide() {
+	var number;
+	do {
+		number = prompt("How many squares per side for your next game?");
+	}
+	while( (number < 2) && (number > 35) );
+	squaresPerSide = number;
+};
+
+function drawTheSquares() {
+	drawingCellDimension = (widthOfField/squaresPerSide) - 2;
+	numberOfSquares = squaresPerSide*squaresPerSide;
+	for (var i = 0; i < numberOfSquares; i++) {
+		$('.playingField').append('<div class="drawingCell"></div>');
+	}
+	// Set the styling for the proper dimensions
+$('.drawingCell').height(drawingCellDimension);	
+$('.drawingCell').width(drawingCellDimension);	
+};
+
+function deleteTheSquares() {
+		$('.drawingCell').remove();
+	};
 });
