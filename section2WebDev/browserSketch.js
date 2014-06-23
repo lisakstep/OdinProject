2e4f21466298af44f225638ef3e655c2d0e4a55e
Lisa Stephens <lisakstep@gmail.com>
2014-06-22 18:56:00 -0600

    add dummy startover button to top center of page

--- section2WebDev/browserSketch.js
+++ section2WebDev/browserSketch.js
@@ -1,17 +1,17 @@
 $(document).ready( function() {
 	// Set up the drawing field and cell parameters
-var numberOfSquares = 1600;
+var numberOfSquares = 16;
 var squaresPerSide = Math.sqrt(numberOfSquares);
 var widthOfField = 500-4;
 var drawingCellDimension = (widthOfField/squaresPerSide) - 2;
 	//alert("the jquery file");
 	//console.log("yep, i'm loaded.");
 // Add the wrapper div to the DOM
-$('body').append("<div class='wrapper'></div>");
+$('.wrapper').append("<div class='playingField'></div>");
 
 // Draw the cells
 for (var i = 0; i < numberOfSquares; i++) {
-	$('.wrapper').append('<div class="drawingCell"></div>');
+	$('.playingField').append('<div class="drawingCell"></div>');
 }
 
 // Set the styling for the proper dimensions
